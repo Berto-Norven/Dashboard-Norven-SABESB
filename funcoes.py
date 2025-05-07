df1d12e08380eeb5f3e746f7a9d9d03d53a7a6f8
Berto <pedro@setrasolucoes.com.br>
2025-05-07 17:02:53 -0300

    Add files via upload

---
+++ funcoes.py
@@ -0,0 +1,175 @@
+# Bibliotecas:
+
+import pandas as pd
+import streamlit as st
+import numpy as np
+import matplotlib as plt
+import plotly.express as px
+from datetime import datetime, timedelta
+import plotly.graph_objects as go
+# -------------------------------------------------------------------------------------------------------------------- #
+def escolha_indice(df):
+    with st.sidebar:
+        st.markdown(
+            "<h3 style='color: #550899; font-family: \"Styrene A Regular\", sans-serif;'>" "2. Selecione os índices:</h3>",
+            unsafe_allow_html=True)
+        st.markdown("""
+                <style>
+                .stMultiSelect [data-baseweb="tag"] {
+                    background-color: #AC4DFF !important;
+                    color: white !important;
+                }
+                .stMultiSelect [data-baseweb="tag"] > div {
+                    color: white !important;
+                }
+                </style>
+                """, unsafe_allow_html=True)
+        escolha_indice = st.multiselect('Escolha o índice para análise:', options=list(df.keys()),default='IPCA')
+    return escolha_indice
+
+def definicao_periodo():
+    with st.sidebar:
+        st.markdown("<h3 style='color: #550899; font-family: \"Styrene A Regular\", sans-serif;'>1. Selecione o intervalo de datas:</h3>", unsafe_allow_html=True)
+        data_inicial, data_final = None, None
+
+        if st.checkbox("Selecionar todo o período disponível"):
+            data_inicial = datetime(1995, 1, 1)
+            data_final = datetime(2025, 3, 1)
+        else:
+            try:
+                data_inicial, data_final = st.date_input(
+                    "Selecione as datas",
+                    value=(datetime(2015, 1, 1), datetime(2025, 3, 1)),
+                    min_value=datetime(1995, 1, 1),
+                    max_value=datetime(2025, 3, 1)
+                )
+            except:
+                st.warning('⚠️ Selecione um período válido.')
+                data_inicial, data_final = None, None
+    return data_inicial, data_final
+
+
+#Função leitura de excel:
+
+def ler_excel(caminho,nome_aba):
+    df = pd.read_excel(caminho,sheet_name = nome_aba)
+    return df
+
+
+def grafico_relevancia(df,col_indice,col_sap,col_relevancia):
+    df.loc[df['Código SAP'] == '-', 'Código SAP'] = df['índices']
+    df['eixo_x'] = 'i- ' + df['Código SAP']
+
+    #Criar gráfico:
+    fig = px.bar(
+        df,
+        x = 'eixo_x',
+        y = col_relevancia,
+        text = col_relevancia,
+        labels={col_relevancia: 'Relevância (%)', 'eixo_x': 'Índice - Código SAP'},
+
+    )
+    # Ajustes de layout
+    fig.update_traces(texttemplate='%{text:.1%}', textposition='outside', marker_color='#550899')
+    fig.update_layout(
+        xaxis_tickangle=-45,  # inclina os rótulos para melhor visualização
+        yaxis_tickformat=".0%",
+        height=700,  # altura mais adequada
+        width=900,
+        margin=dict(t=80, b=150),
+        xaxis=dict(categoryorder='total descending'),
+        font=dict(size=14)
+
+    )
+    return fig
+
+def abilitar_correlação_dolar():
+    st.session_state.button = not st.session_state.button
+
+def grafico_correlação(df):
+    df_variacoes_mensais = df.loc[:,df.columns.str.contains('acumulada_')]
+
+    correlacao_indices =  df_variacoes_mensais.corr()
+
+    fig_correlacao = px.imshow(
+        correlacao_indices,
+        text_auto=True,
+        aspect='auto',
+        title='Correlação entre os índices selecionados'
+    )
+    st.plotly_chart(fig_correlacao,use_container_width=True)
+
+
+def plotar_indice(df,codigo_sap,data_inicial,data_final):
+
+    if 'button' not in st.session_state:
+        st.session_state.button = False
+    with st.sidebar:
+        st.button('Realizar correlção com Dólar', on_click=abilitar_correlação_dolar)
+    if st.session_state.button:
+        codigo_sap =codigo_sap +['Dólar']
+        df = df[['Data'] + codigo_sap]
+    else:
+        df = df[['Data'] + codigo_sap]
+    data_inicial = pd.Timestamp(data_inicial)
+    data_final = pd.Timestamp(data_final)
+    df =  df[['Data']+codigo_sap]
+    try:
+        df = df[(df['Data']>= data_inicial) & (df['Data']<= data_final)]
+        df = df.sort_values('Data').reset_index()
+        for coluna in codigo_sap:
+            fator_acumulado = 1
+            if coluna == 'INCC':
+                df[f'Variação % Mensal_{coluna}'] = df[coluna]
+                df = df.drop(columns=coluna)
+                df[f'Variação % acumulada_{coluna}'] = 0
+                inicio = df[f'Variação % Mensal_{coluna}'].first_valid_index()
+
+
+            else:
+                df[f'Variação % Mensal_{coluna}'] = df[coluna].pct_change() * 100
+
+                df[f'Variação % acumulada_{coluna}'] = 0
+                inicio = df[f'Variação % Mensal_{coluna}'].first_valid_index()
+
+            if inicio is not None:
+                for i in range(inicio, len(df)):
+                    valor = df.loc[i, f'Variação % Mensal_{coluna}']
+                    if pd.notna(valor):
+                        fator_variacao = 1 + (valor / 100)
+                        fator_acumulado *= fator_variacao
+                        df.loc[i, f'Variação % acumulada_{coluna}'] = (fator_acumulado - 1) * 100
+
+        fig_acumulada = go.Figure()
+
+        for coluna in codigo_sap:
+            fig_acumulada.add_trace(go.Scatter(
+                x=df['Data'],
+                y=df[f'Variação % acumulada_{coluna}'],
+                mode='lines+markers',
+                name=f'{coluna} - Acumulado'
+            ))
+        fig_acumulada.update_traces(texttemplate='%{text:.1%}')
+        fig_acumulada.update_layout(
+            #title='Variação Acumulada por Índice',
+            xaxis_title='Data',
+            yaxis_title='Variação % Acumulada (%)',
+            legend_title='Índices',
+            hovermode='x unified',
+            yaxis=dict(
+                ticksuffix='%',
+                tickformat='.0f'
+            )
+        )
+        with st.container():
+            st.subheader("Gráfico de Variação Acumulada")
+            st.plotly_chart(fig_acumulada, use_container_width=True)
+            st.subheader('Tabela de índices')
+            st.dataframe(df, hide_index=True, use_container_width=True)
+            grafico_correlação(df)
+    except:
+        st.warning('⚠️ não existem dados suficientes para o período selecionado.')
+
+
+
+
---
+++ main.py
@@ -0,0 +1,162 @@
+# Autor: Pedro Lucas Berto - Versão: 01 - Data: 30/04/2025 - Norven
+from idlelib.multicall import MC_ENTER
+
+# -------------------------------------------------------------------------------------------------------------------- #
+
+# Bibliotecas:
+
+import pandas as pd
+import streamlit as st
+import numpy as np
+import matplotlib as plt
+import plotly.express as px
+import plotly.express as px
+import plotly.graph_objects as go
+from datetime import datetime, timedelta
+import base64
+
+from altair.vegalite.v5.theme import theme
+from streamlit import sidebar
+
+# -------------------------------------------------------------------------------------------------------------------- #
+
+#Importações adicionais:
+from funcoes import ler_excel, grafico_relevancia,plotar_indice,definicao_periodo,escolha_indice,abilitar_correlação_dolar,grafico_correlação
+
+
+
+
+# -------------------------------------------------------------------------------------------------------------------- #
+
+#Interface:
+if 'active_tab' not in st.session_state:
+    st.session_state.active_tab = 'Sobre'
+
+# Expande a sidebar automaticamente ao mudar para "Análises específicas"
+if st.session_state.active_tab == 'Análises específicas':
+    st.session_state.sidebar_state = 'expanded'
+elif st.session_state.active_tab == 'Sobre':
+    st.session_state.sidebar_state = 'expanded'
+else:
+    st.session_state.sidebar_state = 'collapsed'
+
+
+st.set_page_config(
+    page_title="Dashboard de Índices de Saneamento",
+    page_icon="💧",
+    layout="wide",
+)
+
+
+
+guias = ['Sobre','Relevância do índices','Análises específicas','Conclusões Norven']
+
+st.session_state.active_tab = st.radio("Escolha uma análise:", guias, horizontal=True, label_visibility='collapsed')
+
+
+# -------------------------------------------------------------------------------------------------------------------- #
+
+# Caminhos para leitutra:
+
+caminho_descritivo_indices_atualizacao_monetaria_utilizados = r'C:\Users\pedro.NORVEN\Desktop\DashBoard_SABESB\Arquivos\Tabela_indices_demais_empresas.xlsx'
+
+caminho_indices_atualizacao_monetaria = r'C:\Users\pedro.NORVEN\Desktop\DashBoard_SABESB\Arquivos\Tabela_indices.xlsx'
+
+aba_leitura_caminho_relevancia_indices_atualizacao_monetaria_utilizados = 'Relevância dos índices'
+
+aba_leitura_caminho_indices_atualizacao_monetaria = 'ÍNDICES_PARA_GRÁFICO'
+
+aba_leitura_caminho_descritivo_indices_atualizacao_monetaria_utilizados = 'Descritivo índices'
+
+aba_leitura_conclusoes_descritivo_indices_atualizacao_monetaria_utilizados = 'Conclusões Norven'
+
+# Leitura dos arquivos:
+
+relevancia_indices_atualizacao_monetaria_utilizados = ler_excel(caminho_descritivo_indices_atualizacao_monetaria_utilizados,aba_leitura_caminho_relevancia_indices_atualizacao_monetaria_utilizados)
+
+indices_atualizacao_monetaria = ler_excel(caminho_indices_atualizacao_monetaria,aba_leitura_caminho_indices_atualizacao_monetaria)
+
+descritivo_indices_atualizacao_monetaria_utilizados =ler_excel(caminho_descritivo_indices_atualizacao_monetaria_utilizados,aba_leitura_caminho_descritivo_indices_atualizacao_monetaria_utilizados)
+
+conclusoes_norven = ler_excel(caminho_descritivo_indices_atualizacao_monetaria_utilizados,aba_leitura_conclusoes_descritivo_indices_atualizacao_monetaria_utilizados)
+# -------------------------------------------------------------------------------------------------------------------- #
+if st.session_state.active_tab == 'Relevância do índices':
+
+
+    #Ajuste de layout:
+    col1,col2 =st.columns([3,2])
+
+
+    col_indice = 'índices'
+    col_sap = 'Código SAP'
+    col_relevancia = 'Relevância'
+
+    # descritivo_resumido = descritivo_indices_atualizacao_monetaria_utilizados.drop(columns=['Relevância', 'Qtd.Empresa'])
+
+    grafico_relevancia_indices = grafico_relevancia(relevancia_indices_atualizacao_monetaria_utilizados,col_indice,col_sap,col_relevancia)
+
+
+    with col1:
+        st.subheader('Rpresentatividade de alguns índices utilizados na valoração de ativos no setor de saneamento:')
+        st.plotly_chart(grafico_relevancia_indices,use_container_width=True)
+    with col2:
+        st.subheader('Descritivo dos índices:')
+        st.dataframe(descritivo_indices_atualizacao_monetaria_utilizados,hide_index=True,width=500,height=600,row_height=52)
+
+
+# -------------------------------------------------------------------------------------------------------------------- #
+elif st.session_state.active_tab == 'Análises específicas':
+
+    data_inicial, data_final = definicao_periodo()
+
+    # Ajuste descritivo:
+    descritivo_indices_atualizacao_monetaria_utilizados.loc[
+            descritivo_indices_atualizacao_monetaria_utilizados['Código SAP'] == '-', 'Código SAP'] = \
+            descritivo_indices_atualizacao_monetaria_utilizados['índices']
+
+    chave_nome_indice_codigo_sap = descritivo_indices_atualizacao_monetaria_utilizados.set_index('índices')[
+            'Código SAP'].to_dict()
+
+    escolha_indice = escolha_indice(chave_nome_indice_codigo_sap)
+
+    codigo_sap = [chave_nome_indice_codigo_sap[indice] for indice in escolha_indice]
+
+    plotar_indice(indices_atualizacao_monetaria, codigo_sap, data_inicial, data_final)
+
+elif st.session_state.active_tab == 'Sobre':
+
+    #with st.sidebar:
+        gif_path = r"C:\Users\pedro.NORVEN\Desktop\DashBoard_SABESB\imagens\animação-logo-e-frase.gif"  # Caminho local para o GIF
+
+        with open(gif_path, "rb") as f:
+            data = f.read()
+            encoded = base64.b64encode(data).decode("utf-8")
+
+        st.markdown(
+            f"""
+                <div style="text-align: center;">
+                    <img src="data:image/gif;base64,{encoded}" alt="GIF animado" width=1200">
+                </div>
+                """,
+            unsafe_allow_html=True
+        )
+        with sidebar:
+            st.markdown(
+                "<h3 style='color: #1B1B1B; font-family: \"Styrene A Regular\", sans-serif;'>" "Dashboard Norven: Índices de atualização monetária</h3>",
+                unsafe_allow_html=True)
+
+elif st.session_state.active_tab == 'Conclusões Norven':
+    
+    st.dataframe(
+        conclusoes_norven,
+        hide_index=True,
+        use_container_width=True,
+        height=900,  # define a altura da tabela com scroll se necessário
+        row_height=100  # ajusta a altura de cada linha
+        )
+
+
+
+
+
+
