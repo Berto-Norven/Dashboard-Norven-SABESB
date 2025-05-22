3960153504023760daae3842c048b5588a3c32c6
Berto <pedro@setrasolucoes.com.br>
2025-05-22 17:26:03 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -334,7 +334,7 @@ def plotar_indice(df,codigo_sap,data_inicial,data_final):
         st.warning('⚠️ não existem dados suficientes para o período selecionado.')
 
 def descritivo_especifico(caminho, codigo_aba, indice=0):
-associa_aba_abreviação = {
+    associa_aba_abreviação = {
         "IPCA": "IPCA",
         "INCC": "INCC",
         "INPC": "INPC",
